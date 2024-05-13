5106217709996ef32ca92940cd301176d9d58211
Igor Fernandes <igor.fernandes95@outlook.com>
2024-05-13 13:40:38 -0300

    feito a responsividade da pg

---
+++ HTML - EXERCICIO 09.05/script.js
@@ -0,0 +1,14 @@
+let hamburguer = document.querySelector('.hamburguer');
+let ulNav = document.querySelector('.header__nav__ul');
+
+
+hamburguer.addEventListener("click", () => {
+    ulNav.classList.toggle("active");
+    console.log(ulNav);
+  });
+  
+
+
+
+
+
